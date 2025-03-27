b8209a1656ece0ca08a216d57b4d1e8c1dc633ad
Sina Sedaghat <sina@Sinas-MacBook-Pro.local>
2025-03-27 18:00:45 +0330

    update type annotation for vraiable

--- firstTry.go
+++ firstTry.go
@@ -6,11 +6,11 @@ import (
 )
 
 func main() {
-	var investmentAmount = 2000
-	var years = 10
-	var expectedReturnRate = 5.5
+	var investmentAmount float64 = 2000
+	years := 10.0             // var years float64 = 10
+	expectedReturnRate := 5.5 // var expectedReturnRate = 5.5
 
-	var futureValue = float64(investmentAmount) * math.Pow(1+(expectedReturnRate/100), float64(years))
+	var futureValue = investmentAmount * math.Pow(1+(expectedReturnRate/100), years)
 
 	fmt.Println(futureValue)
 }
