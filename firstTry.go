fc821a683e80d710195cab90d34cc67c2684d2a2
Sina Sedaghat <sina@Sinas-MacBook-Pro.local>
2025-03-27 20:06:31 +0330

    create constant variable

--- firstTry.go
+++ firstTry.go
@@ -6,11 +6,14 @@ import (
 )
 
 func main() {
+	const inflationRate = 3.4
 	var investmentAmount float64 = 2000
 	years := 10.0             // var years float64 = 10
 	expectedReturnRate := 5.5 // var expectedReturnRate = 5.5
 
-	var futureValue = investmentAmount * math.Pow(1+(expectedReturnRate/100), years)
+	futureValue := investmentAmount * math.Pow(1+(expectedReturnRate/100), years)
+	futureRealValue := futureValue / math.Pow(1+(inflationRate/100), years)
 
 	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 }
