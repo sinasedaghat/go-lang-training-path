6c9d9edfd418947e0ebec06002a23375f914d29d
Sina Sedaghat <sina@Sinas-MacBook-Pro.local>
2025-04-01 23:57:50 +0330

    use condition for handle invalid input and use for loop for run again project

--- bank.go
+++ bank.go
@@ -5,36 +5,52 @@ import "fmt"
 func main() {
 	var choice int
 	balance := 1000.0
-
 	fmt.Println("Welcome to Bank 🏦")
-	fmt.Println("What do you want to do?")
-	fmt.Println("1. 💰 Check balance")
-	fmt.Println("2. 💵 Deposit money")
-	fmt.Println("3. 💸 Withdraw money")
-	fmt.Println("4. 👋 Exit")
 
-	fmt.Printf("\n❓ Your choice: ")
-	fmt.Scan(&choice)
+	for i := 0; i < 5; i++ {
+		fmt.Println("What do you want to do?")
+		fmt.Println("1. 💰 Check balance")
+		fmt.Println("2. 💵 Deposit money")
+		fmt.Println("3. 💸 Withdraw money")
+		fmt.Println("4. 👋 Exit")
 
-	if choice == 1 {
-		// println("Your balance is:", balance) // why this code is work? // this print in my terminal ==> Your balance is: +1.000000e+003
-		fmt.Println("💰 Your balance is:", balance)
-	} else if choice == 2 {
-		var input float64
-		fmt.Print("💵 Your deposit: ")
-		fmt.Scan(&input)
-		balance += input
-		fmt.Printf("💰 Your Balance is Update! You have %.2f\n", balance)
-	} else if choice == 3 {
-		var input float64
-		fmt.Print("💸 Withdrawal amount: ")
-		fmt.Scan(&input)
-		balance -= input
-		fmt.Printf("💰 Your Balance is Update! You have %.2f\n", balance)
-	} else if choice == 4 {
-		fmt.Println("Goodby! 👋")
-	} else {
-		fmt.Println("Your choice isn't valid! 😞")
-		fmt.Println("Goodby! 👋")
+		fmt.Printf("\n❓ Your choice: ")
+		fmt.Scan(&choice)
+
+		if choice == 1 {
+			// println("Your balance is:", balance) // Why does this code work? // this print in my terminal ==> Your balance is: +1.000000e+003
+			fmt.Printf("💰 Your balance is: %.2f\n", balance)
+		} else if choice == 2 {
+			var input float64
+			fmt.Print("💵 Your deposit: ")
+			fmt.Scan(&input)
+			if input <= 0 {
+				fmt.Println("‼️ Invalid amount. Must be greater than 0.00!")
+				continue
+			}
+			balance += input
+			fmt.Printf("💰 Your Balance is Update! You have %.2f\n", balance)
+		} else if choice == 3 {
+			var input float64
+			fmt.Print("💸 Withdrawal amount: ")
+			fmt.Scan(&input)
+			if input <= 0 {
+				fmt.Println("‼️ Invalid amount. Must be greater than 0.00!")
+				continue
+			}
+			if input >= balance {
+				fmt.Printf("‼️ Invalid amount. Must be lower than %.2f! (your balance)\n", balance)
+				continue
+			}
+			balance -= input
+			fmt.Printf("💰 Your Balance is Update! You have %.2f\n", balance)
+		} else if choice == 4 {
+			fmt.Println("Goodby! 👋")
+			break
+		} else {
+			fmt.Println("Your choice isn't valid! 😞")
+		}
 	}
+
+	println("🏦 Thanks for choosing our bank.")
 }
