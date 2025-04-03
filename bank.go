082d9869fbe460a07f73d05cc675c1d65ef5cbc9
srm <akbar.agha97@gmail.com>
2025-04-03 07:30:15 +0330

    add read and write file

--- bank.go
+++ bank.go
@@ -3,30 +3,48 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"os"
+	"strconv"
 	"time"
 )
 
+var fileName = "number.txt"
+
+func WriteToFile(number int) {
+	n := fmt.Sprint(number)
+	os.WriteFile(fileName, []byte(n), 0644)
+}
+
+func ReadFile() {
+	number, _ := os.ReadFile(fileName)
+	n := string(number)
+	rt, _ := strconv.Atoi(n)
+	fmt.Printf("random value is %d\n", rt)
+}
 func main() {
 	rand.Seed(time.Now().UnixNano())
 	randomValue := rand.Intn(100)
+	WriteToFile(randomValue)
 	fmt.Println("welcome to New World!")
 	for {
 		input := showMenu()
 		switch input {
 		case 1:
-			fmt.Printf("random value is %d\n", randomValue)
+			ReadFile()
 		case 2:
 			var temp int
 			fmt.Print("enter number:")
 			fmt.Scan(&temp)
 			randomValue += temp
-			fmt.Printf("random value is %d\n", randomValue)
+			WriteToFile(randomValue)
+			ReadFile()
 		case 3:
 			var temp int
 			fmt.Print("enter number:")
 			fmt.Scan(&temp)
 			randomValue -= temp
-			fmt.Printf("random value is %d\n", randomValue)
+			WriteToFile(randomValue)
+			ReadFile()
 		default:
 			fmt.Println("GoodBye!")
 			return
