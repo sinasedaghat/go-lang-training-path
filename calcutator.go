ae13e7a2fa24d86525daa42064763da9b2e05b66
srm <akbar.agha97@gmail.com>
2025-04-09 19:30:28 +0330

    add error hanling

--- calcutator.go
+++ calcutator.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
@@ -8,10 +9,22 @@ func main() {
 	var revenue, expenses, tax_rate float64
 	fmt.Print("enter Revenue: ")
 	fmt.Scanln(&revenue)
+	err := validate(revenue)
+	if err != nil {
+		panic(err)
+	}
 	fmt.Print("enter expenses: ")
 	fmt.Scanln(&expenses)
+	err = validate(expenses)
+	if err != nil {
+		panic(err)
+	}
 	fmt.Print("enter Tax Rate: ")
 	fmt.Scanln(&tax_rate)
+	err = validate(tax_rate)
+	if err != nil {
+		panic(err)
+	}
 	EBT, profit, ratio := calcute(revenue, expenses, tax_rate)
 	fmt.Printf("EBT is: %.0f \n", EBT)
 	fmt.Printf("Profit is: %.0f \n", profit)
@@ -42,4 +55,13 @@ func calcute(revenue float64, expenses float64, tax_rate float64) (EBT float64, 
 	profit = EBT - EBT*(tax_rate/100)
 	ratio = EBT / profit
 	return EBT, profit, ratio
+}
+
+func validate(input float64) error {
+	var err error
+	if input <= 0 {
+		err = errors.New("input not permitted 0 ")
+		return err
+	}
+	return nil
 }
