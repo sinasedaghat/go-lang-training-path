9ac7b541d9c056574df5d4606f2307e79445fd46
Sina Sedaghat <sina@Sinas-MacBook-Pro.local>
2025-04-10 18:36:11 +0330

    complate develop 'tic tac toe' game. 🤘

--- TicTacToe.go
+++ TicTacToe.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 var grid = [][]string{
@@ -9,108 +10,144 @@ var grid = [][]string{
 	{"3", "4", "5"},
 	{"6", "7", "8"},
 }
-var winPattern = [][]int{
-	{1, 2, 3},
-	{1, 4, 7},
-	{1, 5, 9},
-	{2, 5, 8},
-	{3, 6, 9},
-	{3, 5, 7},
-	{4, 5, 6},
-	{7, 8, 9},
-}
-var u1 string
-var u2 string
-var last int // this variable store last selected cell
 
-// var grid = [][]string{
-// 	{"0️⃣", "1️⃣", "2️⃣"},
-// 	{"3️⃣", "4️⃣", "5️⃣"},
-// 	{"6️⃣", "7️⃣", "8️⃣"},
-// }
+var winPattern = [][]int{
+	{0, 1, 2},
+	{0, 3, 6},
+	{0, 4, 8},
+	{1, 4, 7},
+	{2, 5, 8},
+	{2, 4, 6},
+	{3, 4, 5},
+	{6, 7, 8},
+}
+
+var u1 string // X
+var u2 string // O
 
 func main() {
-	winnerChecker(last)
-	fmt.Println(`
-		Welcome to 'Tic Tac Toe' Game
-	`)
+	startUp()
 
-	fmt.Print("Enter first player name (🔵): ")
+	for round := 0; round < 9; round++ {
+		showGrid()
+		cell := getCellNumber(round)
+		setValue(round, cell)
+
+		if winnerChecker(cell) {
+			fmt.Printf("🏆 YO! %v won. 🎉\n", playerPick(round, false))
+			showGrid()
+			return
+		}
+	}
+}
+
+func startUp() {
+	fmt.Println("Welcome To TicTacToe")
+
+	fmt.Print("Enter first player name (X): ")
 	fmt.Scan(&u1)
 
-	fmt.Print("Enter second player name (🔴): ")
+	fmt.Print("Enter second player name (O): ")
 	fmt.Scan(&u2)
 
 	fmt.Println("\nLET'S GO")
-	for i := 0; i <= 9; i++ {
-		showGrid(grid)
-		var row, col int
-		if i%2 == 0 {
-			row, col = selectCell(u1)
-			grid[row][col] = u1
-		} else {
-			row, col = selectCell(u2)
-			grid[row][col] = u2
-		}
-
-		fmt.Printf("row: %v, col: %v\n", row, col)
-	}
 }
 
-func showGrid(grid [][]string) {
-	fmt.Println(grid)
+func showGrid() {
+	fmt.Println("\n ")
 	for row := 0; row < len(grid); row++ {
 		for col := 0; col < len(grid[row]); col++ {
 			if col != 1 {
-				fmt.Printf("| %v |", translateCell(grid[row][col]))
+				fmt.Printf("| %v |", grid[row][col])
 			} else {
-				fmt.Printf(" %v ", translateCell(grid[row][col]))
+				fmt.Printf(" %v ", grid[row][col])
 			}
 		}
-		fmt.Println("\n-------------")
+		if row != 2 {
+			fmt.Println("\n-------------")
+		} else {
+			fmt.Println("\n\n ")
+		}
 	}
 }
 
-func selectCell(player string) (row, col int) {
+func getCellNumber(round int) (cell int) {
 	var message string
-	var cell int
+	var input string
+	player := playerPick(round, false)
+
 	for {
-		if message == "" {
-			fmt.Printf("%v Select cell: ", player)
+		if message != "" {
+			fmt.Println(message)
+			fmt.Print("Select another cell: ")
 		} else {
-			fmt.Printf("%v Select cell: ", message)
+			fmt.Printf("%v select the desired cell: ", player)
 		}
-		fmt.Scan(&cell)
-		row, col = computeCell(cell)
-		if grid[row][col] == "u1" || grid[row][col] == "u2" {
-			message = fmt.Sprintf("Cell %v is already selected, select another cell.", cell)
+
+		fmt.Scan(&input)
+		var err error
+		cell, err = strconv.Atoi(input) // cell, err := strconv.Atoi(input)
+
+		if err != nil {
+			message = "The selected value is not valid, only numbers are valid."
+			continue
+		} else if cell > 9 || cell < 0 {
+			message = "Your selection is not valid, valid cells are between 0 and 9."
+			continue
+		} else if value := cellValue(cell); value == "X" || value == "O" {
+			message = "The desired cell is already selected."
 			continue
 		}
-
 		break
 	}
-	last = cell
-	return
+
+	return cell
 }
 
-func computeCell(cell int) (row, col int) {
-	return cell / 3, cell % 3
-}
-
-func translateCell(cell string) string {
-	switch cell {
-	case u1:
-		return "🔵"
-	case u2:
-		return "🔴"
-	default:
-		return cell
+func playerPick(round int, symbol bool) (player string) {
+	if round%2 == 0 {
+		if symbol {
+			return "X"
+		} else {
+			return u1
+		}
+	} else {
+		if symbol {
+			return "O"
+		} else {
+			return u2
+		}
 	}
 }
 
-func winnerChecker(last int) {
+func cellValue(cell int) (value string) {
+	// check valid cell:  0 <= cell <= 9
+
+	return grid[cell/3][cell%3]
+}
+
+func setValue(round, cell int) {
+	// check valid cell
+
+	grid[cell/3][cell%3] = playerPick(round, true)
+}
+
+func winnerChecker(target int) bool {
+	value := cellValue(target)
+
 	for i := 0; i < len(winPattern); i++ {
-		fmt.Println(winPattern)
+		if pattern := winPattern[i]; includes(pattern, target) && cellValue(pattern[0]) == value && cellValue(pattern[1]) == value && cellValue(pattern[2]) == value {
+			return true
+		}
 	}
-	fmt.Println(last)
+	return false
+}
+
+func includes(array []int, target int) bool {
+	for item := 0; item < len(array); item++ {
+		if array[item] == target {
+			return true
+		}
+	}
+	return false
 }
