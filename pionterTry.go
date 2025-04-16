a4da01c867e2695b4a76a651ea1ab98ac618a445
Sina Sedaghat <sina@Sinas-MacBook-Pro.local>
2025-04-16 07:39:24 +0330

    use pointer in function

--- pionterTry.go
+++ pionterTry.go
@@ -13,4 +13,17 @@ func main() {
 	fmt.Println("this is variable contain pointer of age ==> ", agePointer)
 
 	fmt.Println("this is pointer of age variable ==> ", &age)
+
+	fmt.Printf("this age before call changeValueReference(&age as *int == %v) ===> %v\n", &age, age)
+	changeValueReference(&age)
+	fmt.Printf("this age after call changeValueReference(&age as *int == %v) ===> %v\n", &age, age)
+}
+
+func changeValueReference(age *int) {
+	fmt.Println("this argument of changeValueReference() function", age)
+	fmt.Println("before change age in changeValueReference() function", *age)
+
+	*age = 10
+
+	fmt.Println("after change age in changeValueReference() function", *age)
 }
