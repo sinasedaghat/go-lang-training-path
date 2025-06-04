4820954ea75ebd4e5cccb7e8716ba5c83e85c541
Sina Sedaghat <sina@Sinas-MacBook-Pro.local>
2025-06-04 19:48:25 +0330

    use variadic function and operator, use anonymous function and closure concept

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 this is GoLang Training Path\
-In Maximilian's course, parts 125 to 127\
+In Maximilian's course, parts 125 to 132\
 https://www.udemy.com/course/go-the-complete-guide/
--- functions.go
+++ functions.go
@@ -18,6 +18,36 @@ func main() {
 
 	fmt.Println("equalityDoubleNumbers ==>", equalityDoubleNumbers)
 	fmt.Println("equalityTripleNumbers ==>", equalityTripleNumbers)
+
+	// anonymous function
+	plusFourArray := transformNumber(&normalNumbers, func(number int) int {
+		return number + 4
+	})
+	fmt.Println("plusFourArray ==>", plusFourArray)
+	fmt.Println("normalNumbers ==>", normalNumbers)
+
+	// use closer concept
+	double := transformCreator(2)
+	triple := transformCreator(3)
+
+	doubled := transformNumber(&normalNumbers, double)
+	tripled := transformNumber(&normalNumbers, triple)
+
+	fmt.Println("doubled ==>", doubled)
+	fmt.Println("tripled ==>", tripled)
+
+	// recursion
+	fibonacciResult := fibonacciRecursion(3)
+	fmt.Println("fibonacciResult ==>", fibonacciResult)
+	// fmt.Println("fibonacciResult ==>", fibonacciRecursion(0), fibonacciRecursion(1), fibonacciRecursion(2), fibonacciRecursion(3), fibonacciRecursion(4), fibonacciRecursion(5), fibonacciRecursion(6), fibonacciRecursion(7), fibonacciRecursion(8), fibonacciRecursion(9))
+
+	// aggregate has collect all parameter
+	totalSum := aggregate(1, 2, 3, 4, 5)
+	fmt.Println("totalSum ==>", totalSum)
+
+	//
+	totalNormalNumbers := aggregate(normalNumbers...)
+	fmt.Println("totalNormalNumbers ==>", totalNormalNumbers)
 }
 
 // func transformNumber(numbers *[]int, function func(int) int) []int {
@@ -44,4 +74,30 @@ func double(number int) int {
 
 func triple(number int) int {
 	return number * 3
+}
+
+func transformCreator(factor int) func(int) int {
+	return func(number int) int {
+		return number * factor
+	}
+}
+
+// recursion function
+func fibonacciRecursion(number int) int {
+	if number <= 0 {
+		return 0
+	} else if number == 1 {
+		return 1
+	} else {
+		return fibonacciRecursion(number-1) + fibonacciRecursion(number-2)
+	}
+}
+
+// variadic parameter functions
+func aggregate(numbers ...int) (sum int) {
+	// sum := 0
+	for _, number := range numbers {
+		sum += number
+	}
+	return
 }
