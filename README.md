546fdc68160d13313b109f7366afe66dd853549e
Sina Sedaghat <sina@Sinas-MacBook-Pro.local>
2025-07-22 18:36:13 +0330

    work with goroutines and channels

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 this is GoLang Training Path\
 In Maximilian's course, parts 149 to 158\
 https://www.udemy.com/course/go-the-complete-guide/\
-1.
+1. 
 ```
 func main() {
   channel := make(chan bool)
@@ -16,7 +16,7 @@ func greet(str string, doneChan chan bool) {
 }
 ```
 
-2.
+2. 
 ```
 func main() {
 	channel := make(chan bool)
@@ -42,5 +42,43 @@ func slowGreet(str string, doneChan chan bool) {
 	time.Sleep(3 * time.Second)
 	fmt.Println(str, " ===> HI, from slow greet")
 	doneChan <- true
+}
+```
+3. 
+```
+func main() {
+	for i := range 5 {
+		go sayHello(i)
+	}
+	time.Sleep(1 * time.Second)
+}
+
+func sayHello(id int) {
+	fmt.Printf("Hello from goroutine %d\n", id)
+}
+```
+4. 
+```
+func main() {
+	channel := make(chan int)
+
+	for index := range 5 {
+		go integerPrinter(index, channel)
+	}
+
+	fmt.Println(<-channel)
+	fmt.Println(<-channel)
+	fmt.Println(<-channel)
+	fmt.Println(<-channel)
+	fmt.Println(<-channel)
+
+	integerPrinter(4, channel) // create deadlock
+	fmt.Println(<-channel) 
+}
+
+func integerPrinter(number int, channel chan int) {
+	fmt.Println("I give", number)
+	time.Sleep(time.Duration(number) * time.Second)
+	channel <- number * 3
 }
 ```
--- main.go
+++ main.go
@@ -6,38 +6,37 @@ import (
 )
 
 func main() {
-	// ch1 := make(chan string)
-	channel := make(chan bool)
-	go greet("First", channel)
-	go greet("Second", channel)
-	go slowGreet("First Slow", channel)
-	go greet("Third", channel)
-
-	// valChan := <-channel
-	// fmt.Println(valChan)
-
+	channel := make(chan int)
+	go integerPrinter(1, channel)
 	fmt.Println(<-channel)
-	// <-channel
-	// <-channel
-	// <-channel
-	// <-channel
-	time.Sleep(2 * time.Second) // bad practice
 }
 
-func greet(str string, doneChan chan bool) {
-	fmt.Println(str, " ===> HI, from greet")
-	doneChan <- true
+func integerPrinter(number int, channel chan int) <-chan int {
+	channel2 := make(chan int)
+	fmt.Println("I give", number)
+	time.Sleep(time.Duration(number) * time.Second)
+	channel <- number * 3
+	return channel2
 }
 
-// func greet(str string) <-chan bool {
-// 	channel := make(chan bool)
-// 	fmt.Println(str, " ===> HI, from greet")
-// 	channel <- true
-// 	return channel
+// func sayHello(id int) {
+// 	fmt.Printf("Hello from goroutine %d\n", id)
 // }
 
-func slowGreet(str string, doneChan chan bool) {
-	time.Sleep(3 * time.Second)
-	fmt.Println(str, " ===> HI, from slow greet")
-	doneChan <- true
-}
+// func greet(str string, doneChan chan bool) {
+// 	fmt.Println(str, " ===> HI, from greet")
+// 	doneChan <- true
+// }
+
+// // func greet(str string) <-chan bool {
+// // 	channel := make(chan bool)
+// // 	fmt.Println(str, " ===> HI, from greet")
+// // 	channel <- true
+// // 	return channel
+// // }
+
+// func slowGreet(str string, doneChan chan bool) {
+// 	time.Sleep(3 * time.Second)
+// 	fmt.Println(str, " ===> HI, from slow greet")
+// 	doneChan <- true
+// }
